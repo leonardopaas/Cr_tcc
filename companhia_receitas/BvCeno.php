3ed866c117b1218a4a9d626bf54231d712a3bd54
leonardopaas <32268784+leonardopaas@users.noreply.github.com>
2019-09-24 16:48:02 -0300

    Add files via upload

---
+++ companhia_receitas/BvCeno.php
@@ -0,0 +1,29 @@
+<?php 
+include "cabecalho.php";
+ ?>
+<h1 class="rece">Bolo Vegano de Cenoura</h1>
+<img class="imagem" src="fotos/ceno.jpg">
+<section id="rece">
+  <h1 class="text3">
+  	<br>
+  	Ingredientes:
+  	<br>
+  	<br>
+  	• 2 cenouras grandes;
+  	<br>
+  	• 1/2 de xícara de chá de óleo vegetal;
+  	<br>
+  	• 1 ½ xícara de chá de água;
+  	<br>
+  	• 3 xícaras de chá de farinha de trigo integral;
+  	<br>
+    • 2 xícaras de chá de açúcar de coco;
+    <br>
+    • 2 colheres de sobremesa de fermento em pó;
+    <br>
+    • 1 pitada de sal.
+    <br>
+  	<br>
+  	Unte uma forma e polvilhe com farinha. Pré-aqueça o forno a 190ºC. Em um bowl, misture a farinha, o açúcar e o sal. Pique a cenoura descascada e leve ao liquidificador com a água e o óleo e bata bem até obter uma mistura homogênea. Adicione aos poucos a mistura líquida à mistura seca, mexendo sempre até formar uma massa. Adicione o fermento por último e misture bem sem bater. Despeje a massa na forma e leve ao forno por 45 minutos ou até dourar. Deixe esfriar antes de desenformar e servir.
+  </h1>
+  
---
+++ companhia_receitas/Psl.php
@@ -0,0 +1,35 @@
+<?php 
+include "cabecalho.php";
+ ?>
+<h1 class="rece">Pudim sem leite</h1>
+<img class="imagem" src="fotos/pude.jpg">
+<section id="rece">
+  <h1 class="text3">
+  	<br>
+  	Ingredientes Pudim:
+  	<br>
+  	<br>
+  	• 6 ovos;
+  	<br>
+  	• 1 xícara de chá de açúcar demerara;
+  	<br>
+  	•1 xícara de chá de chocolate em pó solúvel;
+  	<br>
+  	• 1 colher de chá de extrato de baunilha;
+  	<br>
+    • 1 pitada de sal
+    <br>
+    <br>
+    Ingredientes Calda:
+    <br>
+    • 1 xícara de açúcar demerara;
+    <br>
+    • 1/4 xícara de chá de água quente.
+    <br>
+  	<br>
+  	Modo de preparo :
+  	<br>
+  	<br>
+  	Inicie a calda. Adicione o açúcar em uma forma redonda com um furo no meio. Leve ao forno baixo para derreter o açúcar. Quando estiver dourado, adicione água e mexa. Deixe descansar. Enquanto isso peneire as gemas para tirar a película. Bata no liquidificador com os outros ingredientes. Despeje na forma e leve para assar em forno a 180 graus pré-aquecido em banho maria por cerca de 1 hora. Espere esfriar, desenforme e sirva.
+  </h1>
+  
